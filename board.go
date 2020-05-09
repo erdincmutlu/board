3f7682e2bbf00c09ab519869f609d16ae4a20008
Erdinc Mutlu <erdinc@luno.com>
2020-05-09 14:18:00 +0100

    Board has n by m size

--- board.go
+++ board.go
@@ -1,8 +1,25 @@
 package board
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Board() error {
+var IllegalSizeBoard error = errors.New("Board should be at least 1x1")
+
+// The Board structure to be hold
+type Board struct {
+	items      []uint8
+	dimensions []int
+}
+
+// Board will initialize the board with the given dimensions
+func NewBoard(n, m int) (*Board, error) {
+	// Board will have 2 dimensions initially. Then can work on more dimensions
+	if n < 1 || m < 1 {
+		return nil, IllegalSizeBoard
+	}
+
 	fmt.Println("Board initialized")
-	return nil
+	return &Board{items: []uint8{}, dimensions: []int{n, m}}, nil
 }
--- board_test.go
+++ board_test.go
@@ -6,7 +6,18 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestBoard(t *testing.T) {
-	err := Board()
+func TestNewBoard(t *testing.T) {
+	_, err := NewBoard(-1, 5)
+	require.Equal(t, IllegalSizeBoard, err)
+
+	_, err = NewBoard(3, -4)
+	require.Equal(t, IllegalSizeBoard, err)
+
+	expBoard := Board{
+		items:      []uint8{},
+		dimensions: []int{3, 4},
+	}
+	b, err := NewBoard(3, 4)
 	require.NoError(t, err)
+	require.Equal(t, &expBoard, b)
 }
