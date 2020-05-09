83942fb2eca8d9983218a39d10975dcd31db2b9a
Erdinc Mutlu <erdinc@luno.com>
2020-05-09 15:16:16 +0100

    Implemented isInBounds

--- board.go
+++ board.go
@@ -1,11 +1,13 @@
 package board
 
 import (
-	"errors"
 	"fmt"
+
+	"github.com/pkg/errors"
 )
 
-var IllegalSizeBoard error = errors.New("Board should be at least 1x1")
+var ErrIllegalSizeBoard error = errors.New("Board should be at least 1x1")
+var ErrInvalidDimensionsError error = errors.New("Invalid dimensions")
 
 // The Board structure to be hold
 type Board struct {
@@ -13,13 +15,32 @@ type Board struct {
 	dimensions []int
 }
 
-// Board will initialize the board with the given dimensions
+// NewBoard will initialize the board with the given dimensions
 func NewBoard(n, m int) (*Board, error) {
 	// Board will have 2 dimensions initially. Then can work on more dimensions
 	if n < 1 || m < 1 {
-		return nil, IllegalSizeBoard
+		return nil, ErrIllegalSizeBoard
 	}
 
 	fmt.Println("Board initialized")
-	return &Board{items: []uint8{}, dimensions: []int{n, m}}, nil
+	return &Board{
+		items:      []uint8{},
+		dimensions: []int{n, m},
+	}, nil
+}
+
+// Checks whether the given coordinate is in bounds of the board
+func (b *Board) isInBounds(coord []int) (bool, error) {
+	if len(coord) != len(b.dimensions) {
+		msg := fmt.Sprintf("Coordinate %v has %d dimensions; the board has %d",
+			coord, len(coord), len(b.dimensions))
+		return false, errors.Wrap(ErrInvalidDimensionsError, msg)
+	}
+	for i := 0; i < len(coord); i++ {
+		if coord[i] < 0 || coord[i] > b.dimensions[i] {
+			return false, nil
+		}
+	}
+
+	return true, nil
 }
--- board_test.go
+++ board_test.go
@@ -3,15 +3,17 @@ package board
 import (
 	"testing"
 
+	//"github.com/pkg/errors"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 )
 
 func TestNewBoard(t *testing.T) {
 	_, err := NewBoard(-1, 5)
-	require.Equal(t, IllegalSizeBoard, err)
+	require.Equal(t, ErrIllegalSizeBoard, err)
 
 	_, err = NewBoard(3, -4)
-	require.Equal(t, IllegalSizeBoard, err)
+	require.Equal(t, ErrIllegalSizeBoard, err)
 
 	expBoard := Board{
 		items:      []uint8{},
@@ -20,4 +22,57 @@ func TestNewBoard(t *testing.T) {
 	b, err := NewBoard(3, 4)
 	require.NoError(t, err)
 	require.Equal(t, &expBoard, b)
+}
+
+func TestIsInBounds(t *testing.T) {
+	tests := []struct {
+		name     string
+		coord    []int
+		err      error
+		errText  string
+		InBounds bool
+	}{
+		{
+			name:    "Invalid dimension",
+			coord:   []int{1, 2, 3},
+			err:     ErrInvalidDimensionsError,
+			errText: "Coordinate [1 2 3] has 3 dimensions; the board has 2: Invalid dimensions",
+		},
+		{
+			name:     "Ok",
+			coord:    []int{2, 1},
+			InBounds: true,
+		},
+		{
+			name:  "Bigger first",
+			coord: []int{7, 2},
+		},
+		{
+			name:  "Bigger second",
+			coord: []int{2, 8},
+		},
+		{
+			name:  "Negative first",
+			coord: []int{-5, 2},
+		},
+		{
+			name:  "Negative second",
+			coord: []int{1, -3},
+		},
+	}
+
+	b, err := NewBoard(3, 5)
+	require.NoError(t, err)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			inBounds, err := b.isInBounds(test.coord)
+			require.True(t, errors.Is(err, test.err))
+			if err != nil {
+				require.Equal(t, test.errText, err.Error())
+			}
+
+			require.Equal(t, test.InBounds, inBounds)
+		})
+	}
 }
